bdfca666753242f6d3af037c7d435c4d87b4d38b
KT1024-kei <kt0120katu@gmail.com>
2022-09-19 22:19:02 +0900

    add Simulation flow Function and several Experiments

--- Main/Simulation.py
+++ Main/Simulation.py
@@ -10,6 +10,7 @@ sys.path.append('../')
 
 import numpy as np
 
+from tools.Decorator import run_once
 from tools.Mathfunction import Mathfunction, Integration
 from tools.Log import Log_data
 from Drone.Drone_model import Drone, State
@@ -61,12 +62,14 @@ class Simulation(Mathfunction):
                             traj="circle",
                             controller_type="pid",
                             command = "hovering"):
+        controller.switch_controller(controller_type)
         if controller_type == "pid":
             if command =="hovering":
                 P = np.array([0.0, 0.0, 1.0])
                 controller.set_reference(P, V, R, Euler, Wb, Euler_rate, controller_type)    
             elif command == "land":
                 P = np.array([0.0, 0.0, 0.0])
+                controller.set_reference(P, V, R, Euler, Wb, Euler_rate, controller_type) 
             else:
                 controller.set_reference(P, V, R, Euler, Wb, Euler_rate, controller_type)
         elif controller_type == "mellinger":
@@ -85,28 +88,91 @@ class Simulation(Mathfunction):
         drone.Euler_rate.now[1] =  -drone.Euler_rate.now[1]
         
 
-    def main(self):
-        print("main function")
-        t = 0.0
-        M = np.array([0.0, 0.0 ,0.0, 0.0])
-        while self.T_exp > t:
-            
-            for i in range(self.num_drone):
-                self.state_step(self.drones[i])
-                self.logs[i].write_state(t=t, P=self.P, V=self.V, R=self.R, Euler=self.Euler, Wb=self.Wb, Euler_rate=self.Euler_rate*180/np.pi, M=M)
-                
-            
-            for i in range(self.num_drone):
-                self.ctrls[i].set_state(P=self.P, V=self.V, R=self.R, Euler=self.Euler, Wb=self.Wb, Euler_rate=self.Euler_rate)
-                self.ctrls[i].get_output(t)
-                self.ctrls[i].log(self.logs[i], t)
-                
-            for i in range(self.num_drone):
-                M = self.ctrls[i].input_MP_pwm
-                self.drones[i].main(M)
+    def state_step_flow(self, t, M):
 
-            t += self.dt
+        for i in range(self.num_drone):
+            self.state_step(self.drones[i])
+            self.logs[i].write_state(t=t, P=self.P, V=self.V, R=self.R, Euler=self.Euler, Wb=self.Wb, Euler_rate=self.Euler_rate*180/np.pi, M=M)
+    
+    def controll_flow(self, t):
+        for i in range(self.num_drone):
+            self.ctrls[i].set_state(P=self.P, V=self.V, R=self.R, Euler=self.Euler, Wb=self.Wb, Euler_rate=self.Euler_rate)
+            self.ctrls[i].get_output(t)
+            self.ctrls[i].log(self.logs[i], t)
+
+    def Drone_move_flow(self, t):
+        for i in range(self.num_drone):
+            M = self.ctrls[i].input_MP_pwm
+            self.drones[i].main(M)
+
+    def Log_flow(self):
         for i in range(self.num_drone):
             self.logs[i].close_file()
     
-Simulation(10.0, 0.001, 1).main()
+    @run_once
+    def land(self):
+        for i in range(self.num_drone):
+            self.set_reference(controller=self.ctrls[i], command="land")
+    @run_once
+    def hovering(self):
+        for i in range(self.num_drone):
+            self.set_reference(controller=self.ctrls[i], command="hovering")
+    @run_once
+    def track_circle(self):
+        for i in range(self.num_drone):
+            self.set_reference(controller=self.ctrls[i], traj="circle", controller_type="mellinger")
+    @run_once
+    def stop_track(self):
+        for i in range(self.num_drone):
+            self.ctrls[i].stop_tracking()
+
+
+
+    def main(self):
+        print("main function")
+        t = 0.0
+        self.M = np.array([0.0, 0.0 ,0.0, 0.0])
+        while self.T_exp > t:
+            self.state_step_flow(t, self.M)
+            self.controll_flow(t)
+            self.Drone_move_flow(t)
+            t += self.dt
+        self.Log_flow()
+    
+    def hovering_and_land(self):
+        print("main function")
+        t = 0.0
+        self.M = np.array([0.0, 0.0 ,0.0, 0.0])
+        self.hovering()
+        while self.T_exp > t:
+            self.state_step_flow(t, self.M)
+            self.controll_flow(t)
+            self.Drone_move_flow(t)
+            t += self.dt
+            if t > 10:
+                self.land()
+        self.Log_flow()
+    
+    def circle_track_experiment(self):
+        print("main function")
+        t = 0.0
+        self.M = np.array([0.0, 0.0 ,0.0, 0.0])
+        self.hovering()
+        while self.T_exp > t:
+            self.state_step_flow(t, self.M)
+            self.controll_flow(t)
+            self.Drone_move_flow(t)
+            t += self.dt
+            if 15 > t > 5:
+                self.track_circle()
+            if 20 > t > 15:
+                self.stop_track()
+            if t > 20:
+                self.land()
+        self.Log_flow()
+
+            
+    
+
+
+Simulation(25.0, 0.001, 1).circle_track_experiment()
