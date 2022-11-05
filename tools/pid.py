6ee3362782ef9232bd1be6c28384cc81d71cb495
KT1024-kei <kt0120katu@gmail.com>
2022-11-05 16:39:01 +0900

    add limitation parametor of integral term

--- tools/pid.py
+++ tools/pid.py
@@ -1,6 +1,6 @@
 class PID():
 
-  def __init__(self, kp, ki, kd, dt):
+  def __init__(self, kp, ki, kd, dt, Integ_lim = 0):
 
     self.dt = dt
 
@@ -18,17 +18,21 @@ class PID():
 
     self.output = .0
 
+    self.int_limit = Integ_lim
+
   def runpid2(self, state):
 
     self.output = 0.0
     self.Err = self.desired - state
     self.Err_sum += self.Err*self.dt
+    if self.int_limit != 0:
+      self.Err_sum = min(self.int_limit, max(self.Err_sum, -self.int_limit))
     self.Err_div  = (self.Err-self.Err_pre)/self.dt 
     self.Err_pre = self.Err
 
     self.outP = self.kp * self.Err
     self.output += self.outP
-
+    
     self.outD = self.kd * self.Err_div
     self.output += self.outD
 
@@ -40,6 +44,8 @@ class PID():
     self.output = 0.0
     self.Err = self.desired - self.state
     self.Err_sum += self.Err*self.dt
+    if self.int_limit != 0:
+      self.Err_sum = min(self.int_limit, max(self.Err_sum, -self.int_limit))
     self.Err_div  = (self.Err-self.Err_pre)/self.dt 
     self.Err_pre = self.Err
 
